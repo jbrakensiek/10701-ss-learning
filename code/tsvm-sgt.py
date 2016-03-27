30891ff26aaac3b0abb9bd64a96957ac26cbe997
Sidhanth Mohanty <sidhanth@cmu.edu>
2016-03-27 18:24:09 -0400

    coded up a spectral graph transducer as a TSVM optimization method. Will have to run on data to fully debug

--- code/tsvm-sgt.py
+++ code/tsvm-sgt.py
@@ -14,12 +14,12 @@ class tsvm:
         (self.numpoints, self.datadimension) = inputvec.shape
         self.numunlabeled = unlabeledvec.shape
         self.labeled_data = inputvec
+        self.numtotal = self.numunlabeled + self.numpoints
         self.unlabeled_data = unlabelvec
         self.labels = outputvec
-        self.totaldata = np.concatenate(inputvec, unlabeledvec)
-        self.labelcomplete = np.concatenate(outputvec, np.zeros(numunlabeled))
-
-    self.laplacian = np.matrix([])
+        self.totaldata = np.concatenate((inputvec, unlabeledvec))
+        self.labelcomplete = np.concatenate((outputvec, np.zeros(numunlabeled)))
+        self.laplacian = np.matrix([])
     
     ''' Boolean value necessary to specify is objective function should be
         minimized or maximized.
@@ -49,10 +49,9 @@ class tsvm:
         similarity graph.
     '''
     def generateGraph():
-        self.laplacian = np.matrix(np.zeros([self.numpoints +\
-            self.numunlabeled, self.numpoints + numunlabeled]))
+        self.laplacian = np.matrix(np.zeros([self.numtotal, self.numtotal]))
         #find a faster way to do the following with numpy functions
-        for i in range(self.numpoints + self.numlabeled):
+        for i in range(self.numtotal):
             ithvec = self.totaldata[i]
             sum_ithrow = 0
             for j in range(i):
@@ -70,9 +69,24 @@ class tsvm:
 
         #using eigh over eig because it is optimized to work on symmetric
         #matrices
-        laplace_eigval, laplace_eigvec = np.eigh(self.laplacian)
-        laplace_eigval = laplace_eigval * laplace_eigval
+        laplace_eigval, laplace_eigvec = np.linalg.eigh(self.laplacian)
 
+        #some eigenvectors/eigenvalues should potentially be discarded
+        #figure out which ones!
+
+        laplace_eigvec = laplace_eigvec.T
+        #code to sort two arrays together taken from stackoverflow
+        indexes = range(len(laplace_eigval))
+        indexes.sort(key = laplace_eigval.__getitem__)
+        sorted_laplace_eigval = map(laplace_eigval.__getitem__, indexes)
+        sorted_laplace_eigvec = map(laplace_eigvec.__getitem__, indexes)
+        sorted_laplace_eigvec = sorted_laplace_eigvec.T
+
+        #don't have intuition for the following loop. Included it here because
+        #the paper suggested it.
+        dimension_eigval = len(sorted_laplace_eigval)
+        for i in range(dimension_eigval):
+            sorted_laplace_eigval[i] = (i + 1) * (i + 1)
         #the next line because np.sum is faster than any for loop
         sumlabels = np.sum(self.labels)
 
@@ -85,5 +99,64 @@ class tsvm:
         gammaplus = sqrt(float(numnegative)/float(numpositive))
         gammaminus = -1 * sqrt(float(numpositive)/float(numnegative))
 
+        gammavec = np.zeros(self.numtotal)
+        for i in range(self.numpoints):
+            if (self.labels[i] == 1):
+                gammavec[i] = gammaplus
+            else:
+                gammavec[i] = gammaminus
+
         #more stuff to put here
+        costsynthesis = np.zeros(self.numtotal)
+        for i in range(self.numtotal):
+            if (self.labels[i] == 1):
+                costsynthesis[i] = float(numpoints) / (2.0 * float(numpositive))
+            else:
+                costsynthesis[i] = float(numpoints) / (2.0 * float(numnegative))
+        costsynthesis = np.diag(costsynthesis) #this is the 'C' in the paper.
+
+        eigvalmat = np.diag(sorted_laplace_eigval)
+        tradeoff_param = 1 #I set it to 1 arbitrarily, tuning is necessary for
+                           #better performance. This is the 'c' in the paper.
+
+        #same eigenvectors as Laplacian, eigenvalues changed to
+        #gammaplus, gammaminus or 0
+        costweigh_eig = tradeoff_param * np.dot(sorted_laplace_eigvec.T,\
+            costsynthesis)
+
+        #the paper defines a matrix called G. matG represents that matrix
+        matG = eigvalmat + np.dot(costweigh_eig, sorted_laplace_eigvec)
+
+        #the paper defines a vector called b, vecb represents that vector
+        vecb = np.dot(costweigh_eig, gammavec.T)
         
+        numrowsinG = len(matG)
+        negidentity = np.diag(-1 * np.ones(numrowsinG))
+
+        #The following patch of code is to replicate the matrix whose
+        #smallest eigenvalue is of interest to us
+        topPart = np.concatenate((matG.T, negidentity)).T
+        leftbottom = (float(-1)/float(numtotal)) * np.dot(vecb.T, vecb)
+        #matG is the rightbottom
+        bottomPart = np.concatenate((leftbottom.T, matG.t)).T
+        interestmatrix = np.concatenate((topPart, bottomPart))
+
+        ''' - Matrix not symmetric so not sure if eigenvalues are even real
+            - Since I am using eigvals, even if the eigenvalues are real,
+              eigvals might return complex numbers with a small imaginary
+              part because of roundoff error. Hence the map.
+        '''
+        eigvalsOfInterest = np.linalg.eigvals(interestmatrix)
+        eigvalsOfInterest = map(lambda x: sqrt(float((np.real(x) ** 2) +\
+            (np.imag(x) ** 2))), eigvalsOfInterest)
+        #eigenstar is the lambda^* variable
+        eigenstar = min(eigvalsOfInterest)
+        
+        predictionhelper = np.dot(np.dot(sorted_laplace_eigvec,\
+            np.linalg.inv(matG + (eigenstar * negidentity))), bvec)
+
+        #find a better threshold
+        threshold = 0.5 * float(gammaplus + gammaminus)
+
+        predictions = map(lambda x: -1 if x < threshold else 1,\
+            predictionhelper)
